bdaeac77d8cd97da9e1d0f3ae5fdf7c5dbad0df9
Ryan Gomoto <ryan@cyrusbio.com>
2019-01-26 15:47:57 -0800

    Set up unit test framework

--- src/customers/get-customers.spec.ts
+++ src/customers/get-customers.spec.ts
@@ -1,2 +1,7 @@
+import test from 'ava';
 import {getCustomers} from './get-customers';
 
+test('getCustomers should return list of customers', async (t) => {
+  await new Promise(res => setTimeout(res, 2000));
+  t.pass();
+})
