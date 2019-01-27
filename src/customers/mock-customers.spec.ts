ca12f4ada23a0672537dfb927a50cdcd566676a2
Ryan Gomoto <ryan@cyrusbio.com>
2019-01-26 18:51:23 -0800

    Factor out mock

---
+++ src/customers/mock-customers.spec.ts
@@ -0,0 +1,43 @@
+import * as nock from 'nock';
+import { IChargifyCustomer } from '../interfaces';
+import { TestOptions } from '../options.spec';
+
+export interface ICustomerMock {
+  mock: nock.Scope;
+  customers: IChargifyCustomer[];
+}
+
+export function mockCustomers1(options: TestOptions): ICustomerMock {
+  const customers: IChargifyCustomer[] = [
+    {
+      id: 10101,
+      organization: '',
+      first_name: '',
+      last_name: '',
+      email: '',
+      cc_emails: '',
+      reference: '',
+      created_at: '',
+      updated_at: '',
+      address: '',
+      address_2: '',
+      city: '',
+      state: '',
+      zip: '',
+      country: '',
+      phone: '',
+      verified: true,
+      portal_customer_created_at: '',
+      portal_invite_last_sent_at: '',
+      portal_invite_last_accepted_at: '',
+      tax_exempt: true,
+      vat_number: '',
+    }
+  ];
+
+  const mock = nock(`https://${options.chargify.subdomain}.chargify.com`)
+  .get('/customers.json')
+  .reply(200, customers.map(customer => ({customer}))) // wrap each customer object
+
+  return {mock, customers};
+}
