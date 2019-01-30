018e62c37f09c2235ce07e21c841368856668cfe
Ryan Gomoto <ryan@cyrusbio.com>
2019-01-30 02:54:54 -0800

    Add getComponent to ChargifyClient

--- src/chargify-client.ts
+++ src/chargify-client.ts
@@ -1,5 +1,6 @@
-import {getCustomers, IGetCustomersResponse} from './customers/get-customers';
+import {getComponent, IGetComponentRequest, IGetComponentResponse} from './components/get-component';
 import {getComponents, IGetComponentsRequest, IGetComponentsResponse} from './components/get-components';
+import {getCustomers, IGetCustomersResponse} from './customers/get-customers';
 import {getSubscriptions, IGetSubscriptionsResponse} from './subscriptions/get-subscriptions';
 
 export class ChargifyClient {
@@ -9,12 +10,16 @@ export class ChargifyClient {
     this._options = options;
   }
 
-  public async getCustomers(): Promise<IGetCustomersResponse> {
-    return getCustomers(this._options.subdomain, this._options.apiKey)();
+  public async getComponent(input: IGetComponentRequest): Promise<IGetComponentResponse> {
+    return getComponent(this._options.subdomain, this._options.apiKey)(input);
   }
 
   public async getComponents(input: IGetComponentsRequest): Promise<IGetComponentsResponse> {
     return getComponents(this._options.subdomain, this._options.apiKey)(input);
+  }
+
+  public async getCustomers(): Promise<IGetCustomersResponse> {
+    return getCustomers(this._options.subdomain, this._options.apiKey)();
   }
 
   public async getSubscriptions(): Promise<IGetSubscriptionsResponse> {
