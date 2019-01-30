5f1a0f26e9ab7bb7dca0a9da90383f396f237128
Ryan Gomoto <ryan@cyrusbio.com>
2019-01-30 03:10:24 -0800

    Fix indentation

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -60,7 +60,7 @@ export interface IChargifySubscription {
   customer: IChargifyCustomer;
   product: IChargifyProduct;
   state: 'active',
-    balance_in_cents: number;
+  balance_in_cents: number;
   total_revenue_in_cents: number;
   product_price_in_cents: number;
   product_version_number: number;
@@ -78,43 +78,43 @@ export interface IChargifySubscription {
   canceled_at: ChargifyDate;
   current_period_started_at: ChargifyDate;
   previous_state: 'active',
-    signup_payment_id: ChargifyId;
+  signup_payment_id: ChargifyId;
   signup_revenue: string;
   delayed_cancel_at: ChargifyDate;
   coupon_code: null;
   coupon_codes: string[];
   payment_collection_method: 'automatic',
-    snap_day: string | null;
+  snap_day: string | null;
   payment_type: 'credit_card',
-    referral_code: null,
-    next_product_id: null,
-    coupon_use_count: null,
-    coupon_uses_allowed: null,
-    reason_code: null,
-    automatically_resume_at: null,
-    offer_id: null,
-    receives_invoice_emails: null,
-    credit_card: {
-      id: ChargifyId;
-      payment_type: 'credit_card',
-      first_name: string;
-      last_name: string;
-      masked_card_number: string;
-      card_type: string;
-      expiration_month: number;
-      expiration_year: number;
-      billing_address: string | null;
-      billing_address_2: string | null;
-      billing_city: string | null;
-      billing_state: string | null;
-      billing_country: string | null;
-      billing_zip: string | null;
-      current_vault: string;
-      vault_token: string;
-      customer_vault_token: string | null;
-      customer_id: number;
-      disabled: boolean;
-    }
+  referral_code: null,
+  next_product_id: null,
+  coupon_use_count: null,
+  coupon_uses_allowed: null,
+  reason_code: null,
+  automatically_resume_at: null,
+  offer_id: null,
+  receives_invoice_emails: null,
+  credit_card: {
+    id: ChargifyId;
+    payment_type: 'credit_card',
+    first_name: string;
+    last_name: string;
+    masked_card_number: string;
+    card_type: string;
+    expiration_month: number;
+    expiration_year: number;
+    billing_address: string | null;
+    billing_address_2: string | null;
+    billing_city: string | null;
+    billing_state: string | null;
+    billing_country: string | null;
+    billing_zip: string | null;
+    current_vault: string;
+    vault_token: string;
+    customer_vault_token: string | null;
+    customer_id: number;
+    disabled: boolean;
+  }
 }
 
 export interface IChargifyProduct {
@@ -126,33 +126,33 @@ export interface IChargifyProduct {
   price_in_cents: number;
   interval: number;
   interval_unit: 'month',
-    initial_charge_in_cents: null,
-    expiration_interval: number | null;
+  initial_charge_in_cents: null,
+  expiration_interval: number | null;
   expiration_interval_unit: 'never',
-    trial_price_in_cents: number | null;
+  trial_price_in_cents: number | null;
   trial_interval: number | null;
   trial_interval_unit: 'month';
   initial_charge_after_trial: boolean;
   return_params: string;
   request_credit_card: true,
-    require_credit_card: true,
-    created_at: ChargifyDate;
+  require_credit_card: true,
+  created_at: ChargifyDate;
   updated_at: ChargifyDate;
   archived_at: ChargifyDate;
   update_return_url: string;
   tax_code: string;
   update_return_params: string;
   product_family: {
-      id: ChargifyId;
-      name: string;
-      handle: string;
-      accounting_code: string | null;
-      description: string;
-    },
-    public_signup_pages: {
-      id: ChargifyId;
-      url: string;
-    } [],
-    taxable: boolean;
+    id: ChargifyId;
+    name: string;
+    handle: string;
+    accounting_code: string | null;
+    description: string;
+  },
+  public_signup_pages: {
+    id: ChargifyId;
+    url: string;
+  } [],
+  taxable: boolean;
   version_number: number;
 }
